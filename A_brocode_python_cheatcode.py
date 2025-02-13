2d37077aecb26dcd29a48a110ae0af39f1944342
Kiran Kumar Kuntumalla <87245413+Kirankumarkkk@users.noreply.github.com>
2025-02-13 12:47:41 +0530

    Add files via upload

--- A_brocode_python_cheatcode.py
+++ A_brocode_python_cheatcode.py
@@ -1,9 +1,14 @@
+#variables
+
 name="kiran"             #string double quotes
 fullname='kiran kumar k' #string single quotes
 age=10                   #int
 height=5.4               #float
 male=True                #bool
 complexvariable=1+2j     #complex
+
+#list tuples and dict
+
 my_list=[10,2.5,"kiran",3+4j,'kiran kumar kkk']    #list different datatypes
 my_list1=[1,2,3]  #list 
 my_nestedlist=[1,[10,20,30],['a','b','c'],4]  #nested list 
@@ -61,6 +66,7 @@ print(my_dict1.items())  #to print dict_items and key pairs
 print(my_dict1.keys())    #to print dict keys
 print(my_dict1.values())  #to print values
 my_dict1.update({(5,6):'step',(7,8):'engage'}) #update key pair values to the dict
+
 #operators
 print(1+1)  #add
 print(3-1)  #sub
@@ -74,3 +80,24 @@ print(3<6) #less than
 print(3>=3) #greater than or less than
 print(4==4) #comparision operator equal to
 print("hello"!="jello") #not equal to operator
+
+#assignment operator
+assign_x=10
+assign_x += 3 #adding value to variable value and print 
+assign_x -= 3  #substract value to variable value and print
+assign_x /= 5  #division and print value print(assign_X)
+print(assign_x)
+
+#membership operator
+assign_y=[1,2,3,4,5]
+print( 5 in assign_y)
+print( 6 not in assign_y)
+assign_message='hello hi'
+print('w' in assign_message )
+
+#if condition
+
+condition_x=10
+if condition_x==10:             #if condition
+    print("yes condition")
+
